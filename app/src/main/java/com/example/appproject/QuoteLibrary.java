b0f148bf6f0da07f376ab9e871069e8ce3801229
Nathanya Jayasooriya <nathanyajad@gmail.com>
2020-07-25 18:43:53 -0400

    QuoteLibrary (helper to QuotePage)

---
+++ app/src/main/java/com/example/appproject/QuoteLibrary.java
@@ -0,0 +1,13 @@
+package com.example.appproject;
+
+public class QuoteLibrary {
+    private int idxImage;
+
+    public QuoteLibrary(int idxImage) {
+        this.idxImage = idxImage;
+    }
+
+    public int getIdxImage() {
+        return idxImage;
+    }
+}
